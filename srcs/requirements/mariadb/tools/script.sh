574342f7925f6d81330a9efbc9f8c6eb3a9052c9
Imad Mimouni <imimouni@e3r2p13.1337.ma>
2023-09-20 09:56:14 +0100

    update

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# service mariadb start 
-sleep 10
+# service mariadb start #starts mariadb in the background
 
-echo "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;" > db_inc.sql
-echo "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" >> db_inc.sql
-echo "GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%';" >> db_inc.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';" >> db_inc.sql
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_NAME}\`;"
+mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" # % means the host can be any machine or any ip address
+mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
 
-echo "FLUSH PRIVILEGES;" >> db_inc.sql
+mysql -e "FLUSH PRIVILEGES;"
 
-mariadb  < db_inc.sql
+mysqladmin -u root -p$MARIADB_PASSWORD shutdown
+
+exec mysqld_safe
+
+# mariadb  < file
 
 # sed -i  "s/127.0.0.1/0.0.0.0/g"  /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# kill $(cat /var/run/mysqld/mysqld.pid)
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+# service mariadb start #starts mariadb in the background
 
-mysqld
+# mysqld #starts mariadb in the foreground
